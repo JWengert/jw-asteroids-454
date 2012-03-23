d18b85090ae6ef258ca50b1a3db6868918a50d95
Jason Wengert <wengert.jason@gmail.com>
2012-03-23 12:36:42 -0400

    frivolous commentary
    Signed-off-by: Jason Wengert <wengert.jason@gmail.com>

--- Asteroids/Asteroids/Bullet.cs
+++ Asteroids/Asteroids/Bullet.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-
+//frivolous comment
 namespace Asteroids
 {
     /// <summary>
