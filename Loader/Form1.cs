11eab9a375e4a914f026fdab9c47c1794937e031
Jason Wengert <wengert.jason@gmail.com>
2012-04-07 15:38:05 -0400

    lobbies not working yet
    Signed-off-by: Jason Wengert <wengert.jason@gmail.com>

--- Loader/Form1.cs
+++ Loader/Form1.cs
@@ -7,18 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
 
 namespace Loader
 {
-    public partial class Form1 : Form
+    public partial class frmSesSel : Form
     {
-        public Form1()
+        NetworkSession networkSession;
+        AvailableNetworkSessionCollection availableSessions;
+        
+        public frmSesSel()
         {
+            Microsoft.Xna.Framework.GameComponentCollection compcoll = new GameComponentCollection();
+            Asteroids.Game1 game = new Asteroids.Game1();
+            compcoll.Add(new GamerServicesComponent(game));
             InitializeComponent();
+            compcoll[0].Initialize();
+            availableSessions = NetworkSession.Find(
+               NetworkSessionType.SystemLink, 1, null);
+            foreach (AvailableNetworkSession session in availableSessions)
+                lstAvail.Items.Add(session);
+            SignedInGamer.SignedIn +=
+    new EventHandler<SignedInEventArgs>(SignedInGamer_SignedIn);
+        }
+
+        void SignedInGamer_SignedIn(object sender, SignedInEventArgs e)
+        {
+            e.Gamer.Tag = "1";
         }
 
         private void btnLaunch_Click(object sender, EventArgs e)
         {
+
+
             ThreadStart start = new ThreadStart(target);
             Thread gamethread = new Thread(start);
             this.Hide();
