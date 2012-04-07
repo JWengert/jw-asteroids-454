afab36b100735a56d5fadeeebdc74459c7af7fdd
Jason Wengert <wengert.jason@gmail.com>
2012-04-07 14:03:36 -0400

    Form launches game now
    Signed-off-by: Jason Wengert <wengert.jason@gmail.com>

---
+++ Loader/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Loader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart start = new ThreadStart(target);
+            Thread gamethread = new Thread(start);
+            this.Hide();
+            gamethread.Start();
+            gamethread.Join();
+            this.Close();
+            
+        }
+        void target()
+        {
+            Asteroids.Game1 game = new Asteroids.Game1();
+            game.Run();
+        }
+    }
+}
